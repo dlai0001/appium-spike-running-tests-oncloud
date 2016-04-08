66c8dc958d96c050f2350f749f1870721aa42a15
David Lai <dlai@axs.com>
2016-04-08 13:25:06 -0700

    replace run() with POpen

--- shovel/sauce.py
+++ shovel/sauce.py
@@ -1,6 +1,7 @@
 # noinspection PyUnresolvedReferences
 from shovel import task
-from subprocess import PIPE, run
+from subprocess import PIPE
+import subprocess
 import os
 
 @task
@@ -17,6 +18,6 @@ def deploy():
                '--data-binary',
                'staging/TapIt.app.zip'] # file to upload location
     print(' '.join(command))
-    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-    print(result.stdout, result.stderr)
+    result = subprocess.Popen(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    print(result.stdout.read(), result.stderr.read())
 
